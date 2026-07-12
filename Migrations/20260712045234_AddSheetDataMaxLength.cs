71e8148220eae2d6d2625e0912f160e14a295767
K-21 <yocifa9685@namestal.com>
2026-07-12 10:23:01 +0530

    Add database migration for Sheet Data max length

---
+++ Migrations/20260712045234_AddSheetDataMaxLength.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialCalc.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSheetDataMaxLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "Sheets",
+                type: "character varying(5242880)",
+                maxLength: 5242880,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "Sheets",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(5242880)",
+                oldMaxLength: 5242880);
+        }
+    }
+}
